0f4e98cde8bdf9c73845ea35396beaffcdb39860
HuNao <2250529589@qq.com>
2023-05-03 14:18:35 +0800

    添加附魔

--- src/main/java/net/uuz/divinityladder/enchantment/MoonEnchantment.java
+++ src/main/java/net/uuz/divinityladder/enchantment/MoonEnchantment.java
@@ -1,6 +1,7 @@
 package net.uuz.divinityladder.enchantment;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
@@ -9,33 +10,29 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.uuz.divinityladder.Registry.EnchantmentRegistry;
 
 @Mod.EventBusSubscriber
-public class MoonEnchantment extends AttributeEnchantment{
+public class MoonEnchantment extends BaseEnchantment{
     public MoonEnchantment(String name, Rarity rarity, EnchantmentCategory category, EquipmentSlot[] equipmentSlots) {
         super(name, rarity, category, equipmentSlots);
     }
-
-    @Override
-    Attribute getAttribute() {
-        return Attributes.ATTACK_DAMAGE;
-    }
-
     @SubscribeEvent
-    public static void onTickPlayerTick(TickEvent.PlayerTickEvent event) {
-        Player player = event.player;
-        int enchantmentLevel = EnchantmentHelper.getEnchantmentLevel(EnchantmentRegistry.MOON, player);
-        if (enchantmentLevel > 0 && Minecraft.getInstance().level != null){
-            boolean day = Minecraft.getInstance().level.isDay();
-            if(!day){
-                AttributeInstance attribute = player.getAttribute(Attributes.ATTACK_DAMAGE);
-                double value = attribute.getValue() * 0.1;
-                onTickPlayerTick(event, (AttributeEnchantment) EnchantmentRegistry.MOON, value, AttributeModifier.Operation.MULTIPLY_TOTAL);
-
+    public static void onLivingHurt(LivingHurtEvent event) {
+        Entity entity = event.getSource().getEntity();
+        if (entity instanceof Player player){
+            int enchantmentLevel = EnchantmentHelper.getEnchantmentLevel(EnchantmentRegistry.MOON, player);
+            if (enchantmentLevel > 0){
+                Level level = player.level;
+                boolean day = level.isDay();
+                if (!day){
+                    event.setAmount(event.getAmount() + event.getAmount() * 0.1f);
+                }
             }
         }
     }
